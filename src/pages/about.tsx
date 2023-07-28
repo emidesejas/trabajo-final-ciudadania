0c7a7624f2392773dec505243b08eefc834d9cf0
Emiliano <edesejas@eagerworks.com>
2023-07-28 19:49:10 -0300

    feat: update index with welcome text

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,3 @@
-import { Layout } from "~/components/Layout";
-
 export default function About() {
-  return (<Layout>About</Layout>)
-}
+  return ("About")
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,61 +1,34 @@
-import { AnimatePresence } from "framer-motion";
-import { useState } from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 import { Button } from "~/components/ui/button";
-import Slide1 from "~/slides/slide-1";
-import Slide2 from "~/slides/slide-2";
-import Slide3 from "~/slides/slide-3";
-import Slide4 from "~/slides/slide-4";
-import Slide5 from "~/slides/slide-5";
-import Slide6 from "~/slides/slide-6";
-import Slide7 from "~/slides/slide-7";
-import Slide8 from "~/slides/slide-8";
-import Slide9 from "~/slides/slide-9";
-import Slide10 from "~/slides/slide-10";
 
-const TOTAL_SLIDES = 10;
 
 export default function Home() {
-  const [slide, setSlide] = useState(0);
-
-  const nextSlide = () => {
-    if (slide === TOTAL_SLIDES - 1) {
-      setSlide(0);
-    } else {
-      setSlide((current) => current + 1);
-    }
-  };
-
-  const prevSlide = () => {
-    if (slide === 0) {
-      setSlide(TOTAL_SLIDES - 1);
-    } else {
-      setSlide((current) => current - 1);
-    }
-  };
 
   return (
-    <>
-      <main className="flex h-screen w-screen items-center justify-center">
-        <AnimatePresence>
-          {slide === 0 && <Slide1 />}
-          {slide === 1 && <Slide2 />}
-          {slide === 2 && <Slide3 />}
-          {slide === 3 && <Slide4 />}
-          {slide === 4 && <Slide5 />}
-          {slide === 5 && <Slide6 />}
-          {slide === 6 && <Slide7 />}
-          {slide === 7 && <Slide8 />}
-          {slide === 8 && <Slide9 />}
-          {slide === 9 && <Slide10 />}
-        </AnimatePresence>
-        <Button className="absolute bottom-1 right-1" onClick={nextSlide}>
-          Next
-        </Button>
-        <Button className="absolute bottom-1 left-1" onClick={prevSlide}>
-          Prev
-        </Button>
-      </main>
-    </>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      
+      className="text-white flex flex-col mx-3 md:mx-10 lg:mx-60 justify-center items-center gap-3 backdrop-blur md:backdrop-blur-sm bg-white/10 rounded-lg shadow-lg p-5 md:p-10 lg:p-15 md:hover:backdrop-blur transition-all hover:shadow-xl"
+    >
+      <h1 className="font-bold text-2xl md:text-5xl text-center">
+        ¡Hola!
+      </h1>
+      <motion.p className="text-xl md:text-4xl  text-center">
+        Este es el proyecto final del <span className="font-bold">Grupo 40</span> para la materia <span className="font-bold">Construcción de Ciudadanía en Entornos Digitales</span>.
+        Nuestro objetivo es concientizar acerca del uso de las nuevas <span className="font-bold">inteligencias artificiales</span> que están surgiendo y se encuentran en su máximo auge.
+      </motion.p>
+      <div className="text-black flex flex-col sm:flex-row gap-2 sm:gap-10 mt-3 items-center">
+        <Link href={`/about`}>
+          <Button className="md:text-xl transition-all hover:scale-110 active:scale-105 active:bg-slate-300" size="lg" variant="outline" >Saber más del proyecto</Button>
+        </Link>
+        <Link href={`/steps/1-initial`}>
+          <Button className="md:text-xl transition-all hover:scale-110 active:scale-105 active:bg-slate-300" size="lg" variant="outline" >Realizar el tour</Button>
+        </Link>
+      </div>
+    </motion.div>
   );
 }
